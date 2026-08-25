cfcaa7359342c3eb7445c07b22e3bf57fb2803f9
Manoa <manoa@livenexx.net>
2026-08-25 17:25:54 +0300

    INIT git

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,98 +1,179 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import * as SQLite from "expo-sqlite";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
+type SQLiteDatabase = SQLite.SQLiteDatabase | null;
+interface Note {
+  id: number;
+  title: string;
+  createdAt: string;
 }
 
-export default function HomeScreen() {
+let db: SQLiteDatabase = null;
+
+//Open or create database
+
+async function initDatabase(): Promise<SQLiteDatabase> {
+  try {
+    db = await SQLite.openDatabaseAsync("mydb.db");
+    await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS notes (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT NOT NULL,
+          createdAt TEXT NOT NULL
+        );
+      `);
+    return db;
+  } catch (error) {
+    console.log("failed to open or create database");
+
+    return null;
+  }
+}
+
+export default function Index() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+
+  const fetchNotes = async () => {
+    if (!db) {
+      console.error("Error: DB not found");
+      return;
+    }
+    try {
+      const allNotes: Note[] = (await db!.execAsync(
+        `SELECT * FROM notes ORDER BY createdAt DESC`,
+      )) as any;
+
+      setNotes(allNotes);
+    } catch (error) {
+      console.error("Error when fetching all data");
+    }
+  };
+
+  const addNote = async () => {
+    if (!db || !title.trim()) return;
+    try {
+      const now = new Date().toISOString();
+      await db.runAsync(`INSERT INTO notes (title, createdAt) VALUES (?,?);`, [
+        title.trim(),
+        now,
+      ]);
+
+      setTitle("");
+      fetchNotes();
+    } catch (error) {
+      console.error("Error : ", error);
+    }
+  };
+
+  const init = () => {
+    setIsLoading(true);
+    initDatabase()
+      .then(() => {
+        console.log("Databases ready");
+        fetchNotes();
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Notes app</Text>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Write note"
+          style={styles.input}
+          value={title}
+          onChangeText={setTitle}
+        />
+        <TouchableOpacity style={styles.button} onPress={addNote}>
+          <Text>Add</Text>
+        </TouchableOpacity>
+      </View>
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
-    </ThemedView>
+      <View>
+        {/* Notes List */}
+        <FlatList
+          data={notes}
+          keyExtractor={(item: Note) => item.id.toString()}
+          renderItem={({ item }) => {
+            return (
+              <View
+                style={{
+                  backgroundColor: "#fff",
+                  padding: 15,
+                  borderRadius: 12,
+                  marginBottom: 12,
+                  shadowColor: "#000",
+                  shadowOffset: { width: 0, height: 2 },
+                  shadowOpacity: 0.1,
+                  shadowRadius: 6,
+                  elevation: 3,
+                }}
+              >
+                <>
+                  <Text style={{ fontSize: 17, marginBottom: 6 }}>
+                    {item.title}
+                  </Text>
+                </>
+              </View>
+            );
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
+    padding: 20,
   },
   title: {
-    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 800,
+    marginBottom: 10,
   },
-  code: {
-    textTransform: 'uppercase',
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
   },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    borderRadius: 10,
+    marginBottom: 10,
+    fontSize: 14,
+    flex: 1,
+    marginTop: 4,
+    height: "auto",
+  },
+  button: {
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#c3c3c3",
+    borderRadius: 10,
   },
 });
